01c05805107eedf57b70bc49facd2a78fed4ee1f
Annie Bjurman <id19abn@cs.umu.se>
2023-05-09 15:38:56 +0200

    update honeycomb

--- mental_health_app/components/Honeycomb.tsx
+++ mental_health_app/components/Honeycomb.tsx
@@ -1,16 +1,42 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 export default function Honeycomb() {
     return (
-        <div style={styles.honeycomb}>Honeycomb</div>
+        <>
+            <View style={[styles.honeycombDefault, styles.honeycombTop]}></View>
+            <View style={[styles.honeycombDefault, styles.honeycombMiddle]}>hej</View>
+            <View style={[styles.honeycombDefault, styles.honeycombBottom]}></View>
+        </>
     );
 }
 
 const styles = StyleSheet.create({
-    honeycomb: {
-        height: 100,
-        width: 100,
-        borderWith: 10,
-        backgroundColor: 'black'
+    honeycombDefault: {
+        width: 0,
+        borderColor: 'green',
     },
+    honeycombTop: {
+        borderTopStyle: 'none',
+        borderBottomWidth: 30,
+        borderLeftColor: 'transparent',
+        borderLeftWidth: 52,
+        borderRightColor: 'transparent',
+        borderRightWidth: 52,
+    },
+    honeycombBottom: {
+        borderBottomStyle: 'none',
+        borderTopWidth: 30,
+        borderLeftColor: 'transparent',
+        borderLeftWidth: 52,
+        borderRightColor: 'transparent',
+        borderRightWidth: 52,
+    },
+    honeycombMiddle: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 104,
+        height: 60,
+        backgroundColor: 'green'
+    }
 });
