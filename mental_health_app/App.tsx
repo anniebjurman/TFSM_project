59ab0c85ae54b445479e0d1277a355a6992825d3
Karl Lambertsson <id19kln@cs.umu.se>
2023-05-09 12:05:46 +0200

    Added TaskButton component

--- mental_health_app/App.tsx
+++ mental_health_app/App.tsx
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import TaskButton from './components/TaskButton';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <h1>Button</h1>
+      <TaskButton 
+        border="none"
+        color="pink"
+        height = "52px"
+        onClick={() => console.log("You clicked on next!")}
+        radius = "25px"
+        width = "149.83px"
+        children = "hello"
+
+      />
       <StatusBar style="auto" />
     </View>
   );
