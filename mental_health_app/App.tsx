b629ce6e441edce2b6e37caab3087241f71ec761
Linnea <esberg.linnea@gmail.com>
2023-05-09 10:49:02 +0200

    Add App.css

--- mental_health_app/App.tsx
+++ mental_health_app/App.tsx
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import './App.css';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <h1>Hej!</h1>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -13,7 +15,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
