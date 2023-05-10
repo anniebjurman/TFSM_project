ee251eb0aa4bc5610bd52e5be3db09a96881f674
Linnea <esberg.linnea@gmail.com>
2023-05-10 13:42:33 +0200

    Add clickable hearts to QA bubbles

--- mental_health_app/components/QABubble.tsx
+++ mental_health_app/components/QABubble.tsx
@@ -1,25 +1,61 @@
-import { StyleSheet, View, Text, Image } from 'react-native';
+import { View, Image, StyleSheet, Pressable, Text, Switch, TouchableOpacity } from 'react-native';
 import styles from '../App.style';
+import React, {useState} from 'react';
+
 
 export default function QABubble() {
     return (
         <View style={styles2.bubble}>
             <Text style={styles.h3}>Question: How to make friends?</Text>
             <Text style={styles.p}>Answer: At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum....</Text>
-            <Image style={styles2.heart} source={require('../assets/QA/heart.png')}></Image>
+
+            <SwitchImage/>
         </View>
     )
 }
 
+const SwitchImage = () => {
+    const [pressed, setPressed] = useState(false);
+    const like_uri1 = require('../assets/QA/heart.png');
+    const like_uri2 = require('../assets/QA/heart_filled.png');
+  
+    const pressLike = () => {
+      setPressed(!pressed);
+    };
+  
+    return (
+      <TouchableOpacity style={styles2.touchable} onPress={pressLike}>
+        <Image
+          source={pressed ? like_uri2 : like_uri1}
+          style={styles2.heart}
+        />
+      </TouchableOpacity>
+    );
+  };
+
+
 const styles2 = StyleSheet.create({
     bubble: {
-        padding: 25,
+        marginTop: 20,
+        paddingTop: 20,
+        paddingHorizontal: 20,
+        paddingBottom: 15,
         width: '90%',
         backgroundColor: '#FFCC88',
         borderRadius: 20
     },
+
+    touchable: {
+        flex:0, 
+        alignSelf:'flex-end'
+    },
+
     heart: {
+        marginTop: 10,
+        marginBottom: 0,
         width: 30,
-        height: 30
+        height: 30,
+        resizeMode: 'contain',
+        alignSelf: 'flex-end'
     }
 })
