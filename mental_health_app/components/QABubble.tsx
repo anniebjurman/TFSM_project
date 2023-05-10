0e7b6d507123677fc77c71283c35560e597ba518
Linnea <esberg.linnea@gmail.com>
2023-05-10 12:00:55 +0200

    add heart images for QA

--- mental_health_app/components/QABubble.tsx
+++ mental_health_app/components/QABubble.tsx
@@ -1,19 +1,25 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native';
+import styles from '../App.style';
 
 export default function QABubble() {
     return (
-        <View style={styles.bubble}>
-            <h3>Question: How to make friends?</h3>
-            <p>Answer: At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum....</p>
+        <View style={styles2.bubble}>
+            <Text style={styles.h3}>Question: How to make friends?</Text>
+            <Text style={styles.p}>Answer: At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum....</Text>
+            <Image style={styles2.heart} source={require('../assets/QA/heart.png')}></Image>
         </View>
     )
 }
 
-const styles = StyleSheet.create({
+const styles2 = StyleSheet.create({
     bubble: {
-        padding: 20,
-        width: '80%',
+        padding: 25,
+        width: '90%',
         backgroundColor: '#FFCC88',
-        borderRadius: 30
+        borderRadius: 20
+    },
+    heart: {
+        width: 30,
+        height: 30
     }
 })
