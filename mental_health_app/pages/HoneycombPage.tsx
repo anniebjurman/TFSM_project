56480043c466d63b9b14d347c9ffa6df0ef4ae11
Annie Bjurman <id19abn@cs.umu.se>
2023-05-11 20:43:25 +0200

    init honeycomb page

---
+++ mental_health_app/pages/HoneycombPage.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, View, Text, Image, ImageBackground } from 'react-native';
+import styles from '../App.style';
+
+export default function Honeycomb(props: any) {
+    const theme: string = props.theme;
+
+    return (
+        <></>
+    );
+
+}
