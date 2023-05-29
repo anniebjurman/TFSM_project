a5fa6eae59ee79d4c888f0065861843e0d45fe16
Linnea <esberg.linnea@gmail.com>
2023-05-29 19:02:48 +0200

    Add expand functionality to the QA-bubble

--- mental_health_app/components/QABubble.tsx
+++ mental_health_app/components/QABubble.tsx
@@ -1,60 +1,72 @@
-import { View, Image, StyleSheet, Pressable, Text, Switch, TouchableOpacity } from 'react-native';
+import { View, Image, StyleSheet, Pressable, Text, Switch, TouchableOpacity, Animated } from 'react-native';
 import styles from '../App.style';
-import React, {useState} from 'react';
-
+import React, { useCallback, useEffect, useState } from 'react';
 
 export default function QABubble(props) {
-    return (
-        <View style={styles2.bubble}>
-            <Text style={styles.h3}>{props.question}</Text>
-            <Text style={styles.p}>Answer: {props.answer}</Text>
-            <SwitchImage/>
-        </View>
-    )
+  const [textShown, setTextShown] = useState(false); //To show remaining Text
+  const toggleNumberOfLines = () => { //To toggle the show text or hide it
+    setTextShown(!textShown);
+  }
+
+  return (
+    <View style={styles2.bubble}>
+      <Text style={styles.h3}>{props.question}</Text>
+      {/* <Text style={styles.p}>Answer: {props.answer}</Text> */}
+
+      <TouchableOpacity onPress={toggleNumberOfLines}>
+        <Text numberOfLines={textShown ? undefined : 3} style={styles.p}>
+          Answer: {props.answer}
+        </Text>
+      </TouchableOpacity>
+      <SwitchImage />
+    </View>
+  )
 }
 
+// Switch between filled or unfilled heart
 const SwitchImage = () => {
-    const [pressed, setPressed] = useState(false);
-    const like_uri1 = require('../assets/QA/heart.png');
-    const like_uri2 = require('../assets/QA/heart_filled.png');
-  
-    const pressLike = () => {
-      setPressed(!pressed);
-    };
-  
-    return (
-      <TouchableOpacity style={styles2.touchable} onPress={pressLike}>
-        <Image
-          source={pressed ? like_uri2 : like_uri1}
-          style={styles2.heart}
-        />
-      </TouchableOpacity>
-    );
+  const [pressed, setPressed] = useState(false);
+  const like_uri1 = require('../assets/QA/heart.png');
+  const like_uri2 = require('../assets/QA/heart_filled.png');
+
+  const pressLike = () => {
+    setPressed(!pressed);
   };
+
+  return (
+    <TouchableOpacity style={styles2.touchable} onPress={pressLike}>
+      <Image
+        source={pressed ? like_uri2 : like_uri1}
+        style={styles2.heart}
+      />
+    </TouchableOpacity>
+  );
+};
 
 
 const styles2 = StyleSheet.create({
-    bubble: {
-        marginTop: 20,
-        paddingTop: 20,
-        paddingHorizontal: 20,
-        paddingBottom: 15,
-        width: '90%',
-        backgroundColor: '#FFCC88',
-        borderRadius: 20
-    },
+  bubble: {
+    marginTop: 20,
+    paddingTop: 20,
+    paddingHorizontal: 20,
+    paddingBottom: 15,
+    width: '90%',
+    height: 'auto',
+    backgroundColor: '#FFCC88',
+    borderRadius: 20,
+    overflow: 'hidden'
+  },
 
-    touchable: {
-        flex:0, 
-        alignSelf:'flex-end'
-    },
+  touchable: {
+    flex: 0,
+    alignSelf: 'flex-end'
+  },
 
-    heart: {
-        marginTop: 10,
-        marginBottom: 0,
-        width: 30,
-        height: 30,
-        resizeMode: 'contain',
-        alignSelf: 'flex-end'
-    }
+  heart: {
+    marginBottom: 0,
+    marginTop:10,
+    width: 30,
+    height: 30,
+    resizeMode: 'contain',
+  }
 })
