ec7f15a79d4d57d03b042de49c5f42971feedd38
mali4u <malin.leander99@gmail.com>
2023-05-29 14:02:16 +0200

    App.tsx

--- mental_health_app/App.tsx
+++ mental_health_app/App.tsx
@@ -1,7 +1,8 @@
 import { View, Text, Image } from 'react-native';
-import styles from './App.style';
 import { useFonts, Quicksand_400Regular, Quicksand_700Bold } from '@expo-google-fonts/quicksand';
 import * as SplashScreen from 'expo-splash-screen';
+import {NavigationContainer } from '@react-navigation/native';
+import NavigationBar from './components/NavigationBar'
 
 SplashScreen.preventAutoHideAsync();
 
@@ -21,14 +22,9 @@ export default function App() {
   // A regular return of the element
   // NOTE: Use <Text> for all text element, and add the right style for h1 etc instead
     return (
-    <>
-      <View style={[styles.container, styles.body]}>
-        <Text style={styles.h1}>Hi!</Text>
-        <Text style={styles.p}>Open up App.js to start working on your app!</Text>
-        <Text style={styles.h2}>Welcome</Text>
-        <Text style={styles.p}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus, doloremque ad a corrupti</Text>
-      </View>
-    </>
+      <NavigationContainer>
+        <NavigationBar/>
+      </NavigationContainer>
   );
   }
 }
